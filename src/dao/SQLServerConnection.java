088ccd6c6ce7823f51f2ebc2db723c46861d79fc
LD-Tien <leductien00@gmail.com>
2022-04-21 22:06:08 +0700

    add

---
+++ src/dao/SQLServerConnection.java
@@ -0,0 +1,41 @@
+package dao;
+
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * HoTen: LeDucTien
+ * MSV: 2050531200309
+ * LHP: 221LTJNC01
+ */
+
+public class SQLServerConnection {
+    public static Connection getSQLServerConnection(){
+        String connectionURL = "jdbc:jtds:sqlserver://localhost:1433/NganHangJavanc";
+        String userName = "sa";
+        String password = "12345sa";
+        
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            return DriverManager.getConnection(connectionURL, userName, password);
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(SQLServerConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }    
+        ////  Kiểm tra kêt nối DB
+//    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        Connection conn = getSQLServerConnection();
+//        
+//        System.out.println("Get connection " + conn);
+//        if (conn != null)
+//            System.out.println("Done!");
+//        else
+//            System.out.println("Error!");
+//    }
+}
