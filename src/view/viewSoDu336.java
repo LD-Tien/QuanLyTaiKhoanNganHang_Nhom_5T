f2027cdbce0da967cbe8ab83522c0561d98ab479
levanthang23092002 <levanthang230902@gmail.com>
2022-04-27 08:33:08 +0700

    update giao dien

--- src/view/viewSoDu336.java
+++ src/view/viewSoDu336.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package edu.poly.atmjavanc.view;
+package view;
 
 import edu.poly.atmjavanc.helper.databasehelper;
 import java.sql.Connection;
@@ -215,6 +215,8 @@ String sql ="SELECT * FROM Account where MaThe = ? ";
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(viewSoDu336.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
